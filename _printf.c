3b3da6e51cba0a07ce2a24d1f22c5f57514ee2a6
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-21 04:07:54 -0700

    _printf.c-percenatge_and_space_only_edge_case

--- _printf.c
+++ _printf.c
@@ -87,6 +87,8 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
+			if (format[1] == ' ' && format[2] == '\0')
+				return (-1);
 			found_handler = 0;
 			for (j = 0; j < num_handlers; j++)
 			{
