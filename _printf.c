cd1d1ba386ad8faf19182f5d14f06efb1ca1157a
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-21 04:41:42 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -90,6 +90,7 @@ int _printf(const char *format, ...)
 			if (format[i] == ' ' && format[i + 1] != '\0')
 			{
 				return (-1);
+				break;
 			}
 			found_handler = 0;
 			for (j = 0; j < num_handlers; j++)
