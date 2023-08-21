18a57b416f077c0c0c46a5ee2fd6cda10216f7b6
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-21 07:31:22 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -81,7 +81,7 @@ int _printf(const char *format, ...)
 	};
 
 	num_handlers = sizeof(handlers) / sizeof(handlers[0]);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 	va_start(args, format);
 
