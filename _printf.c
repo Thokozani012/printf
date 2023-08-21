8aab292bba174e56b545a40c656a8c8b43cf3670
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-21 12:36:30 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,123 +1,56 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include "main.h"
 
 /**
- * handle_char - Prints characters
- * @args: variable argument
- * @counter: counts the number of char printed
- *
- * Return: Nothing (void function)
- */
-void handle_char(va_list args, int *counter)
-{
-	counter += _putchar(va_arg(args, int));
-}
-
-/**
- * handle_string - Prints strings
- * @args: variable arg
- * @counter: counts the no of char printed
- *
- * Return: Nothing (void function)
- */
-void handle_string(va_list args, int *counter)
-{
-	char *str;
-
-	str = va_arg(args, char *);
-	if (str == NULL)
-		str = "NULL";
-	while (*str)
-	{
-		*counter += _putchar(*str);
-		str++;
-	}
-}
-
-/**
- * handle_percent - Prints a percentage sign
- * @args: variadic arg
- * @counter: counts the number of charaters printed
- *
- * Return: Nothing (void function)
- */
-void handle_percent(va_list args, int  *counter)
-{
-	(void) args;
-	*counter += _putchar('%');
-}
-
-/**
- * handle_default - Prints a default character if none of speciefies match
- * @args: variable argument
- * @counter: counts the number of characters printed
- * @c: Character to be printed
- *
- * Return: Nothing (void function)
- */
-void handle_default(va_list args, int *counter, char c)
-{
-	(void) args;
-	*counter += _putchar(c);
-}
-
-/**
  * _printf - Printf function replica
- * @format: Format argument(s)
+ * @format: Format arguments
  *
- * Return: Number of printed characters
+ * Return: Number of characters printed to the stdout
  */
+
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i, j, num_handlers, found_handler, counter = 0;
-	FormatHandler handlers[] = {
-		{'c', handle_char},
-		{'s', handle_string},
-		{'%', handle_percent},
-		{0, NULL}
-	};
+	int counter = 0;
+	char *str;
 
-	num_handlers = sizeof(handlers) / sizeof(handlers[0]);
-	if (format == NULL)
-		return (-1);
 	va_start(args, format);
 
-	for (i = 0; format[i]; i++)
+	if (format == NULL)
+		return (-1);
+	for (; *format; ++format)
 	{
-		if (format[i] == '%')
+		if (*format != '%')
 		{
-			i++;
-			if (format[i] == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			else if (format[i] == ' ')
-			{
-				va_end(args);
-				return (-1);
-			}
-			found_handler = 0;
-			for (j = 0; j < num_handlers; j++)
-			{
-				if (format[i] == handlers[j].type)
-				{
-					handlers[j].handler(args, &counter);
-					found_handler = 1;
-					break;
-				}
-			}
-			if (!found_handler)
-			{
-				if (format[i] != '%')
-					counter += _putchar('%');
-				handle_default(args, &counter, format[i]);
-			}
+			counter += _putchar(*format);
 		}
 		else
-			counter += _putchar(format[i]);
+		{
+			++format;
+			if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "NULL";
+				while (*str)
+				{
+					counter += _putchar(*str);
+					str++;
+				}
+			}
+			else if (*format == 'c')
+			{
+				counter += _putchar(va_arg(args, int));
+			}
+			else
+			{
+				if (*format != '%')
+					counter += _putchar('%');
+				counter += _putchar(*format);
+			}
+		}
 	}
 	va_end(args);
 	return (counter);
