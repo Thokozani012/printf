899bad8e3636aab462aea592763a7ce0f277b2bd
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-21 04:15:45 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -87,7 +87,7 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[1] == ' ' && format[2] == '\0')
+			if (format[i] == ' ' && format[i + 1] == '\0')
 				return (-1);
 			found_handler = 0;
 			for (j = 0; j < num_handlers; j++)
