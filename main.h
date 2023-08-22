dbc867c28738e381a9ca9107ba81d68b33ffc7d0
@Thokozani012 <Thokozanidoctor123@gmail.com>
2023-08-22 11:58:31 -0700

    main.h-updated

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#define BUFFER_SIZE 1024
+
 #include <stdarg.h>
 
 /**
@@ -18,9 +20,13 @@ typedef struct handle
 
 int _putchar(char c);
 void handle_char(va_list args, int *counter);
-void handle_string(va_list args, int *counter);
+void handle_string(va_list args, char *buffer, int *buffer_index);
 void handle_percent(va_list args, int *counter);
 void handle_default(va_list args, int *counter, char c);
+int count_digits(int n);
+void process_integer(int num, char *buffer, int *buffer_index);
+void check_and_flush_buffer(char *buffer, int *buffer_index);
+void flush_remaining_buffer(char *buffer, int *buffer_index);
 int _printf(const char *format, ...);
 
 #endif /*_MAIN_H*/
